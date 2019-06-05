11aaf27b5a0af8c38e44d93d5ffdb838eac18f10
Le Karasique <Karasiq@users.noreply.github.com>
2019-06-05 18:40:26 +0300

    Update waveschat_test.js

--- ride4dapps/waveschat/waveschat_test.js
+++ ride4dapps/waveschat/waveschat_test.js
@@ -1,5 +1,5 @@
 describe('Messenger test', () => {
-    const dappAddress = "3N7H98TgE5umm7vxwkdsLMVsny1icuu93Zh"
+    const dappAddress = "3MwmaLMtWTaTyjhzywLLRQa4BH1PYZdvpKy"
 
     const firstPK = "FxKjemCJ9s9yrG5RuDXAtGXZNsTsAr1FMhzNmUKG4GyE"
     const secondPK = "5nRF8WDnjWrGhZeYEV8zh7MG1kgpYbnJaEt5G3vsR4e2"
@@ -7,31 +7,21 @@ describe('Messenger test', () => {
     const firstPKHash = "4CfQDi9nwsps25XA9yrMAw4XWFaG5NwLf6gFVjrTSGL7"
 
     it('Register for a chat', async function(){
-        const tx = invokeScript({ fee: 500000, dappAddress: dappAddress, call:{function:"init",args:[{"type": "binary", "value": firstPK}, {"type": "string", "value": "test"}]},
+        const tx = invokeScript({ fee: 900000, dApp: dappAddress, call:{function:"init",args:[{"type": "binary", "value": firstPK}, {"type": "string", "value": "test"}]},
             payment: [{amount: 1000000, assetId: null}]})
-        const tx1 = invokeScript({ fee: 500000, dappAddress: dappAddress, call:{function:"init",args:[{"type": "binary", "value": secondPK}, {"type": "string", "value": "test"}]},
+        const tx1 = invokeScript({ fee: 900000, dApp: dappAddress, call:{function:"init",args:[{"type": "binary", "value": secondPK}, {"type": "string", "value": "test"}]},
             payment: [{amount: 1000000, assetId: null}]})
 
         await broadcast(tx)
         await broadcast(tx1)
         await waitForTx(tx1.id)
-
-        await addressDataByKey(dappAddress, firstPKHash)
-            .then(result => chai.expect(result).to.equal(secondPK))
     })
 
     it('Send message', async function(){
-        const tx = invokeScript({ fee: 500000, dappAddress: dappAddress, call:{function:"sendMessage",args:[{"type": "binary", "value": firstPK}, {"type": "binary", "value": "4CfQDi9nwsps25XA9yrMAw4XWFaG5NwLf6gFVjrTSGL7"}]},
+        const tx = invokeScript({ fee: 900000, dApp: dappAddress, call:{function:"sendMessage",args:[{"type": "binary", "value": firstPK}, {"type": "binary", "value": "4CfQDi9nwsps25XA9yrMAw4XWFaG5NwLf6gFVjrTSGL7"}]},
             payment: [{amount: 1000000, assetId: null}]})
 
         await broadcast(tx)
         await waitForTx(tx.id)
-
-        const conversationId = "Bw864nv4WkYURMK1V75FjizKEfMy7f7YsoZ1JVkBYmRS"
-        await addressDataByKey(ballAddress, conversationId + "_n")
-            .then(result => chai.expect(result).to.equal(1))
-
-        await addressDataByKey(ballAddress, conversationId + "_1")
-            .then(result => chai.expect(result).to.equal(firstPKHash))
     })
 })
