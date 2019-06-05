84fe8de8c226c8a11194a8b9098325e5af540b34
Le Karasique <Karasiq@users.noreply.github.com>
2019-06-05 18:48:55 +0300

    Update mixer_test.js

--- ride4dapps/mixer/mixer_test.js
+++ ride4dapps/mixer/mixer_test.js
@@ -1,18 +1,12 @@
 describe('Mixer test', () => {
-    const dappAddress = "3Musn2zFuw3G71yg6G7PDRAq7CjWxK7Z4pk"
+     const dappAddress = "3N3st6Cp9ZLz8kmT33EY41AVjwKqBVebvyq"
 
-    const hash = "8N5gCoRKcCD4BxJNGn2kKUzMBieetEh4r7DUMHa1m2gq"
+      const hash = "8N5gCoRKcCD4BxJNGn2kKUzMBieetEh4r7DUMHa1m2gq"
 
-    it('Deposit funds', async function(){
-        const tx = invokeScript({ fee: 500000, dappAddress: dappAddress, call:{function:"deposit",args:[{"type": "binary", "value": hash}]},
-            payment: [{amount: 1000000, assetId: null}]})
-        await broadcast(tx)
-        await waitForTx(tx.id)
-    })
-
-    it('Withdraw funds', async function(){
-        const tx = invokeScript({ fee: 500000, dappAddress: dappAddress, call:{function:"withdraw",args:[{"type": "integer", "value": 3}]}, payment: null)
-        await broadcast(tx)
-        await waitForTx(tx.id)
-    })
-})
+      it('Deposit funds', async function(){
+         const tx = invokeScript({ fee: 900000, dApp: dappAddress, call:{function:"deposit",args:[{"type": "binary", "value": hash}]},
+             payment: [{amount: 1000000, assetId: null}]})
+         await broadcast(tx)
+         await waitForTx(tx.id)
+     })
+ })
