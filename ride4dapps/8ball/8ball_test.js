fa2f0a63f8ddeb84966f85c598f42b08cbbf7994
Grigory Ivanov <givanov@wavesplatform.com>
2019-06-06 10:36:13 +0300

    Updated invoke call format Fixed tests Some formatting

--- ride4dapps/8ball/8ball_test.js
+++ ride4dapps/8ball/8ball_test.js
@@ -1,11 +1,11 @@
 //
-// Waves DApp Magic 8 ball tests
+// Waves dApp Magic 8 ball tests
 //
 
 describe('8 ball', () => {
     const ballAddress = "3N27HUMt4ddx2X7foQwZRmpFzg5PSzLrUgU"
     const question = "Test" + Date.now()
-    const tx = invokeScript({ fee: 500000, dappAddress: ballAddress, call:{function:"tellme",args:[{"type": "string", "value": question}]}, payment: null})
+    const tx = invokeScript({fee: 500000, dApp: ballAddress, call:{function:"tellme", args:[{"type": "string", "value": question}]}, payment: null})
 
     it('Tx is mined in block', async function(){
         await broadcast(tx)
@@ -13,7 +13,7 @@ describe('8 ball', () => {
     })
 
     it('Question is in ball', async function(){
-        await addressDataByKey(ballAddress, address()+"_q")
-            .then(reslove => chai.expect(reslove).to.equal(question))
+        await accountDataByKey(address()+"_q", ballAddress)
+            .then(reslove => expect(reslove.value).to.equal(question))
     })
 })
