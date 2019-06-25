ebfa9857d68d82bdc2d50a9599021cee1c7821c4
Le Karasique <Karasiq@users.noreply.github.com>
2019-06-25 17:12:11 +0300

    Update mixer_test.js

--- ride4dapps/mixer/mixer_test.js
+++ ride4dapps/mixer/mixer_test.js
@@ -1,5 +1,5 @@
 describe('Mixer test', () => {
-     const dappAddress = "3N3st6Cp9ZLz8kmT33EY41AVjwKqBVebvyq"
+     const dappAddress = "3MwmaLMtWTaTyjhzywLLRQa4BH1PYZdvpKy"
 
       const hash = "8N5gCoRKcCD4BxJNGn2kKUzMBieetEh4r7DUMHa1m2gq"
 
